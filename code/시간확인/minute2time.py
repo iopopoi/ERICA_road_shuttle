9499fa353cf287de25472d6245b901b9d09b73d6
iopopoi <kimkin0668@gmail.com>
2019-11-17 22:39:31 +0900

    주어진 minute을 00:00의 형태로 출력해주는 함수

--- code/시간확인/minute2time.py
+++ code/시간확인/minute2time.py
@@ -1,3 +1,10 @@
 def minute2time(minute):
-    return str((int)((minute-minute%60)/60))+":"+str(minute%60)
-# print(minute2time(610)) #testcase 입니담
+    hour=(int)(((minute-minute%60)/60)%24)
+    mit=minute%60
+    s_hour=str(hour)
+    s_mit=str(mit)
+    if(hour<10) : s_hour ="0"+s_hour
+    if(mit<10) : s_mit ="0"+s_mit
+    return s_hour+":"+s_mit
+
+print(minute2time(0))
