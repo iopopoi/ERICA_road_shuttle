22ef3bd91e0eac004c8a18489312c5defb5eacd6
hihiroo <alsrud606@naver.com>
2019-11-19 18:17:12 +0900

    특정 날짜에서 몇시간 후의 날짜, 몇분 후의 날짜 리턴하는 함수 추가

--- code/Time_Check/Date.py
+++ code/Time_Check/Date.py
@@ -5,15 +5,28 @@ from datetime import datetime, date, timedelta
 def today(): #오늘 날짜를 리턴
     return datetime.today()
 
+
+def deltaminute(delta,start_day = -1): #특정 날짜에서 몇 분 후, 뒤의 날짜를 리턴
+    if start_day == -1:
+        start_day = today()
+    return start_day + timedelta(minutes = delta)
+
+
+def deltahour(delta,start_day = -1): #특정 날짜에서 몇 시간 후, 뒤의 날짜를 리턴 
+    if start_day == -1:
+        start_day = today()
+    return start_day + timedelta(hours = delta)
+
+
 def deltaday(delta, start_day = -1): #특정 날짜에서 몇일 후,전의 날짜를 리턴
     if start_day == -1: #디폴트 값은 오늘 날짜
         start_day = today()
     return start_day + timedelta(delta)
 
-#오늘 날짜를 입력받아 주말인지 판단하는 함수
-def IsWeekend(time = -1):
+
+def IsWeekend(time = -1): #오늘 날짜를 입력받아 주말인지 판단하는 함수
     if time == -1: # 디폴트 값이면 오늘 날짜로
-        time = today()
+        time = today().day
 
     week = time.weekday() #현재 시간을 불러옴
     if week == 5 or week == 6: #토요일이거나 일요일이면 참
@@ -21,8 +34,8 @@ def IsWeekend(time = -1):
     else:
         return False
 
-# 주어진 minute을 "00:00"의 형태로 출력해주는 함수
-def minute2time(minute):
+
+def minute2time(minute): # 주어진 minute을 "00:00"의 형태로 출력해주는 함수
     hour = (int)(((minute-minute%60)/60)%24)
     mit = minute%60
     s_hour = str(hour)
@@ -36,6 +49,6 @@ def minute2time(minute):
     return s_hour + ":" + s_mit
 
 
-#시간과 분을 입력받아 분으로 바꾸는 함수
-def Tominute(hour,minute):
+def Tominute(hour,minute): #시간과 분을 입력받아 분으로 바꾸는 함수
     return hour*60+minute
+
