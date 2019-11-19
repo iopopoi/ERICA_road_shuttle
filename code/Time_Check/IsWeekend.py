31a43d6737612764f79ff092bbed43ca3d80397b
hihiroo <alsrud606@naver.com>
2019-11-19 15:56:49 +0900

    오늘이 주말인지 확인하는 방식에서 주어진 날짜가 주말인지 확인하는 방식으로 수정

--- code/Time_Check/IsWeekend.py
+++ code/Time_Check/IsWeekend.py
@@ -1,9 +1,12 @@
 #오늘 날짜를 입력받아 주말인지 판단하는 함수
-
+from Date import today
 from datetime import datetime
 
-def IsWeekend():
-    week = datetime.today().weekday() #현재 시간을 불러옴
+def IsWeekend(time = -1):
+    if time == -1: # 디폴트 값이면 오늘 날짜로
+        time = today()
+
+    week = time.weekday() #현재 시간을 불러옴
     if week == 5 or week == 6: #토요일이거나 일요일이면 참
         return True
     else:
