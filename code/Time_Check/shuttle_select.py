71a8bab0c11d46279a380cd81532d8217f000213
iopopoi <kimkin0668@gmail.com>
2019-11-19 15:33:45 +0900

    셔틀 시간표(셔틀콕>한대앞,셔틀콕>기숙사)를 보충하고 이를 받아올 수 있는 함수로 수정

--- code/Time_Check/shuttle_select.py
+++ code/Time_Check/shuttle_select.py
@@ -1,17 +1,27 @@
 import os
 from IsWeekend import IsWeekend
+from tominute import Tominute
 
 
 def shuttle_select(time, station):
     txt_path = os.path.dirname(__file__) #현재 경로 저장
 
     if IsWeekend() == False: #평일일때
-        if station == "shuttlecok": #탑승 위치가 셔틀콕이면
+        if station == "shuttlecok_to_sub": #도착지가 한대앞일때
             txt_path = os.path.join(txt_path,"shuttlecok_to_subwaystation_weekday.txt") #텍스트 위치 경로에 추가
+        elif station == "shuttlecok_to_dom":
+            txt_path = os.path.join(txt_path,"shuttlecok_to_dom_weekday.txt") #텍스트 위치 경로에 추가
         else:
+            #txt_path = os.path.join(txt_path,"dom_to_shuttlecok_weekday.txt") #텍스트 위치 경로에 추가
             return -1
     else:
-        return -1 #아직 셔틀시간표 미완으로 남겨둠
+        if station == "shuttlecok_to_sub": #도착지가 한대앞일때
+            txt_path = os.path.join(txt_path,"shuttlecok_to_subwaystation_weekend.txt") #텍스트 위치 경로에 추가
+        elif station == "shuttlecok_to_dom":
+            txt_path = os.path.join(txt_path,"shuttlecok_to_dom_weekend.txt") #텍스트 위치 경로에 추가
+        else:
+            #txt_path = os.path.join(txt_path,"dom_to_shuttlecok_weekend.txt") #텍스트 위치 경로에 추가
+            return -1
     
 
     infile = open(txt_path,"r") #파일 읽음
@@ -20,13 +30,13 @@ def shuttle_select(time, station):
     S = text.partition(":") 
     SS = S[2].partition(",")
     text = SS[2]
-    chk_timetable = int(S[0])*60 + int(SS[0])
+    chk_timetable = Tominute(int(S[0]),int(SS[0]))
 
     while(time>chk_timetable):
         S = text.partition(":")
         SS = S[2].partition(",")
         text = SS[2]
-        chk_timetable = int(S[0])*60 + int(SS[0])
+        chk_timetable = Tominute(int(S[0]),int(SS[0]))
         if text == "":
             return -1
         
