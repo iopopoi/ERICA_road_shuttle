83830d8967033089f13c0ad99ccbcb6f2378374d
iopopoi <kimkin0668@gmail.com>
2019-11-19 21:15:15 +0900

    출발지와 도착지에 따라 셔틀시간표를 불러오는 함수 추가 및 기존 코드 수정

--- code/Time_Check/shuttle_select.py
+++ code/Time_Check/shuttle_select.py
@@ -1,46 +1,55 @@
 import os
-from IsWeekend import IsWeekend
-from tominute import Tominute
+from Date import *
 
-
-def shuttle_select(time, station):
+def Find_txt_path(time, station, destination):
     txt_path = os.path.dirname(__file__) #현재 경로 저장
-
-    if IsWeekend() == False: #평일일때
-        if station == "shuttlecok_to_sub": #도착지가 한대앞일때
-            txt_path = os.path.join(txt_path,"shuttlecok_to_subwaystation_weekday.txt") #텍스트 위치 경로에 추가
-        elif station == "shuttlecok_to_dom":
-            txt_path = os.path.join(txt_path,"shuttlecok_to_dom_weekday.txt") #텍스트 위치 경로에 추가
-        else:
-            #txt_path = os.path.join(txt_path,"dom_to_shuttlecok_weekday.txt") #텍스트 위치 경로에 추가
-            return -1
-    else:
-        if station == "shuttlecok_to_sub": #도착지가 한대앞일때
-            txt_path = os.path.join(txt_path,"shuttlecok_to_subwaystation_weekend.txt") #텍스트 위치 경로에 추가
-        elif station == "shuttlecok_to_dom":
-            txt_path = os.path.join(txt_path,"shuttlecok_to_dom_weekend.txt") #텍스트 위치 경로에 추가
-        else:
-            #txt_path = os.path.join(txt_path,"dom_to_shuttlecok_weekend.txt") #텍스트 위치 경로에 추가
-            return -1
+    txt_path = os.path.join(txt_path,"timetable")
     
+    if IsWeekend(time) == False: # 평일일 때
+        if station == "shuttlecok": #셔틀콕 출발
+            if destination == "subwaystation":#한대앞 도착
+                txt_path = os.path.join(txt_path,"shuttlecok_to_subwaystation_weekday.txt") #텍스트 위치 경로에 추가
+            elif destination == "dom": #긱사 도착
+                txt_path = os.path.join(txt_path,"shuttlecok_to_dom_weekday.txt") #텍스트 위치 경로에 추가
+            else:
+                #txt_path = os.path.join(txt_path,"dom_to_shuttlecok_weekday.txt") #텍스트 위치 경로에 추가
+                return -1
 
+    else: # 주말일 때
+        if station == "shuttlecok": #셔틀콕 출발
+            if destination == "subwaystation": #한대앞 도착
+                txt_path = os.path.join(txt_path,"shuttlecok_to_subwaystation_weekend.txt") #텍스트 위치 경로에 추가
+            elif destination == "dom": #긱사 도착
+                txt_path = os.path.join(txt_path,"shuttlecok_to_dom_weekend.txt") #텍스트 위치 경로에 추가
+            else:
+                #txt_path = os.path.join(txt_path,"dom_to_shuttlecok_weekend.txt") #텍스트 위치 경로에 추가
+                return -1
+    
+    return txt_path
+
+
+def shuttle_select(time, station, destination):
+    txt_path = Find_txt_path(time,station,destination) #타야하는 셔틀시간표 경로 찾음
     infile = open(txt_path,"r") #파일 읽음
     text = infile.read()
 
-    S = text.partition(":") 
-    SS = S[2].partition(",")
-    text = SS[2]
-    chk_timetable = Tominute(int(S[0]),int(SS[0]))
+    S = text.partition(":")
+    SS = S[2].partition(",") 
+    text = SS[2] # 
+    
+    shuttle_time = make_time(int(SS[0]),int(S[0]),time)
 
-    while(time>chk_timetable):
+    while(Tominute(time) > Tominute(shuttle_time)): # 분으로 환산하여 비교 후 이미 떠난 셔틀이면 패스
         S = text.partition(":")
         SS = S[2].partition(",")
         text = SS[2]
-        chk_timetable = Tominute(int(S[0]),int(SS[0]))
-        if text == "":
+
+        shuttle_time = make_time(int(SS[0]),int(S[0]),time) #셔틀 시간을 날짜 형식으로 바꿈
+
+        if text == "": #모든 셔틀이 떠나갔으면
             return -1
         
     infile.close()
-    return chk_timetable 
+    return shuttle_time
 
-print(shuttle_select(2014,"shuttlecok"))
+print(shuttle_select(now(),"shuttlecok","subwaystation"))
