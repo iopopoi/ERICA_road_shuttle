d6fe1d0b906f15530f826ee35c43bf360446a960
hihiroo <alsrud606@naver.com>
2019-12-04 00:47:12 +0900

    code / folium_search_spot.py : 마커 팝업 추가, gps기능 추가 마커를 클릭하면 위치 정보가 나오도록 팝업 추가, 왼쪽 gps버튼을 클릭하면 현재 위치가 보이도록 추가

--- GUI/folium_search_spot.py
+++ GUI/folium_search_spot.py
@@ -15,6 +15,13 @@ points = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [longti['셔틀콕'], lati['셔틀콕']]
+            },
+            "style": {
+                "icon": {
+                    "iconUrl": "http://downloadicons.net/sites/default/files/small-house-with-a-chimney-icon-70053.png",
+                    "iconSize": [1, 1],
+                    "iconAnchor": [8, 8]
+                }
             }
         },
         {
@@ -36,13 +43,133 @@ points = {
                 "type": "Point",
                 "coordinates": [longti['제3공학관'], lati['제3공학관']]
             }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "제4공학관 "
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['제4공학관'], lati['제4공학관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "제5공학관 "
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['제5공학관'], lati['제5공학관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "컨퍼런스홀"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['컨퍼런스홀'], lati['컨퍼런스홀']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "학연산 클러스터 "
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['학연산 클러스터'], lati['학연산 클러스터']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "제2과학기술관 "
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['제2과학기술관'], lati['제2과학기술관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "제1학술관"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['제1학술관'], lati['제1학술관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "학생복지관"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['학생복지관'], lati['학생복지관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "학생회관"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['학생회관'], lati['학생회관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "학술정보관"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['학술정보관'], lati['학술정보관']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "기숙사 셔틀콕"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['기숙사 셔틀콕'], lati['기숙사 셔틀콕']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "창업보육센터"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['창업보육센터'], lati['창업보육센터']]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "한대앞역"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [longti['한대앞역'], lati['한대앞역']]
+            }
         }
     ]
 }
 
 m = folium.Map(
     location=[37.298215, 126.837191],
-    zoom_start=15
+    zoom_start=16
 )
 
 
