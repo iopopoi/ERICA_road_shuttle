75c2a2c82752fd88c884e6adff1c369c85edb6d5
iopopoi <kimkin0668@gmail.com>
2019-12-06 15:58:23 +0900

    code / Dijkstra.py : 셔틀시간표를 불러와 이동시간 탐색 shuttle_select함수를 불러와 셔틀을 타고 이동하는 경우를 계산가능하게 코드 수정

--- code/Time_Check/Dijkstra.py
+++ code/Time_Check/Dijkstra.py
@@ -1,5 +1,9 @@
 import heapq
 from spot import *
+from Date import *
+from shuttle_select import *
+from datetime import datetime, date, timedelta
+
 # 셔틀시간을 확인하는 기능 미완성
 def Dijkstra(station, destination):
     heap =[]
@@ -8,7 +12,7 @@ def Dijkstra(station, destination):
     visit_from = [ 0 for i in range(50)]
     heapq.heappush(heap,( 0 , (station , station) ) )
     visit_from[station]=station
-    while heap is not None:
+    while heap != []:
         vtx = heap[0][1][0]
         cnt = heap[0][0]
         x=heap[0][1][1]
@@ -20,10 +24,27 @@ def Dijkstra(station, destination):
         for tpl in spot_list[vtx] :
             next= tpl[0]
             if  visit[next] == 0 :
-                if building.count(next) > 0 and next != destination:
-                    heapq.heappush( heap, (cnt + tpl[1] +1, (next , vtx)))
-                else :    
-                    heapq.heappush( heap, (cnt + tpl[1], (next , vtx)))
+                if vtx ==12 or vtx==38 or vtx ==43:
+                    time = deltaTime(((cnt-cnt%60)/60)%24,cnt%60,0)
+                    if vtx == 12 :
+                        sstation ="shuttlecok"
+                        if next == 43 : ddestination="subwaystation"
+                        elif next == 38 : ddestination="dom"
+                    elif vtx == 38:
+                        sstation ="dom"
+                        if next == 12: ddestination="shuttlecok"
+                    # 한대앞역에서 학교로 오는 부분 구현해야됨
+                    # elif vtx == 43:
+                    #     sstation ="subwaystation"
+                    #     if 12 : ddestination = "shuttlecok"
+                    shuttle_time = shuttle_select(time, sstation, ddestination)
+                    shuttle_time = Tominute(shuttle_time)-Tominute(now())
+                    heapq.heappush( heap, ( shuttle_time+tpl[1] , (next , vtx) ) )
+                else :
+                    if building.count(next) > 0 and next != destination:
+                        heapq.heappush( heap, (cnt + tpl[1] +1, (next , vtx)))
+                    else :    
+                        heapq.heappush( heap, (cnt + tpl[1], (next , vtx)))
     spot_load = []
     s = destination
     while visit_from[s] != s :
@@ -33,5 +54,5 @@ def Dijkstra(station, destination):
     spot_load.reverse()
     return cnt, spot_load
 
-print(Dijkstra(33,1))
-print(Dijkstra(26,20))
+print(Dijkstra(33,43))
+
