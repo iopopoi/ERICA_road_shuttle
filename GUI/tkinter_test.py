43a9db9f7c7928ba1c510e42dbdbfa0e5df07ed0
hihiroo <alsrud606@naver.com>
2019-12-11 16:27:11 +0900

    code / tkinter_test.py : 출도착지 선택 콤보박스 추가 + __file__변수가 주피터에서 에러떠서 abspath()로 변경

--- GUI/tkinter_test.py
+++ GUI/tkinter_test.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import font
+from tkinter import ttk
 import os
 import webbrowser
+import folium_search_spot
 
 window = tk.Tk()
 window.title("ERICA Road Shuttle")
@@ -10,8 +12,8 @@ window.configure(background='White')  # 바탕 색
 
 
 def view_map_B_event():
-    url = os.path.dirname(__file__)
-    url = os.path.join(url,"example.html")
+    url = os.path.abspath("")
+    url = os.path.join(url, "spot_search.html")
 
     webbrowser.open(url)
 
@@ -21,6 +23,21 @@ font = tk.font.Font(family="맑은 고딕", size=15, weight='bold')  # 폰트
 view_map_B = tk.Button(window, text="장소 검색", font=font, fg='White', bg='green',
                        command=view_map_B_event)
 view_map_B.place(x=210, y=100, width=250, height=50)  # 라벨 위치 및 크기
+
+
+#출도착지 선택 콤보박스
+values = ['셔틀콕', '제1공학관', '제3공학관', '제4공학관',
+          '제1학술관', '컨퍼런스홀', '제2과학기술관', '학생복지관',
+          '학생회관', '학술정보관', '제5공학관', '창업보육센터',
+          '학연산 클러스터', '기숙사 셔틀콕']
+start_point = tk.ttk.Combobox(window, height=20, values=values)
+start_point.place(x=250, y=200)
+start_point.set("           출발지 선택")
+
+values.append('한대앞역')
+end_point = tk.ttk.Combobox(window, height=20, values=values)
+end_point.place(x=250, y=250)
+end_point.set("           도착지 선택")
 
 
 tk.mainloop()
