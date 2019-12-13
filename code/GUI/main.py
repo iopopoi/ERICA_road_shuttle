44ae69277771e166f015e22aaa485d68f811f8a6
iopopoi <kimkin0668@gmail.com>
2019-12-13 17:13:21 +0900

    code : main.py:gui 도착 예정시간 추가 gui 디자인 변경

--- code/GUI/main.py
+++ code/GUI/main.py
@@ -2,11 +2,14 @@ import tkinter as tk
 from tkinter import font,ttk
 import os
 import webbrowser
-import folium_search_spot
+# import folium_search_spot
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))) + "\Time_Check")
 import Dijkstra
-import folium_draw_line
+# import folium_draw_line
+from datetime import datetime, date, timedelta
+from Date import *
+import math
 
 window = tk.Tk()
 window.title("ERICA Road Shuttle")
@@ -15,7 +18,7 @@ window.configure(background='White')  # 바탕 색
 
 
 def view_map_B_event():
-    folium_search_spot.make_html()
+    # folium_search_spot.make_html()
     url = os.path.abspath("")
     url = os.path.join(url, "spot_search.html")
 
@@ -30,19 +33,26 @@ def View_path():
         return False
     
     (arrival_time,path_list) = Dijkstra.dijkstra(start,end)
-    print(path_list)
-    folium_draw_line.draw_path(path_list)
+
+    arrive_time = math.ceil(arrival_time)
+    hour = (arrive_time-arrive_time%60)/60
+    arrive_time = deltaTime(hour , arrive_time%60 , 0 )
+    arrival_text = "도착 예정 시간은 "+str(arrive_time.hour)+"시 "+str(arrive_time.minute)+"분 입니다."
+    arrive_label = tk.Label( window , text = arrival_text , bg = "white" , width = 32 , height = 1 , font = ("맑은 고딕",15) ,  bd = 1 )
+    arrive_label.place(x=162,y=240)
+    # folium_draw_line.draw_path(path_list)
     url = os.path.abspath("")
     url = os.path.join(url,"path_line.html")
 
-    webbrowser.open(url)
+    #webbrowser.open(url)
     
 
+
 #장소검색 버튼
-font = tk.font.Font(family="맑은 고딕", size=15, weight='bold')  # 폰트
+font = tk.font.Font(family="맑은 고딕", size=16, weight='bold')  # 폰트
 view_map_B = tk.Button(window, text="장소 검색", font=font, fg='White', bg='green',
                        command=view_map_B_event)
-view_map_B.place(x=210, y=100, width=250, height=50)  # 라벨 위치 및 크기
+view_map_B.place(x=210, y=100, width=265, height=50)  # 라벨 위치 및 크기
 
 
 #출도착지 선택 콤보박스
@@ -51,18 +61,18 @@ values = ['셔틀콕', '제1공학관', '제3공학관', '제4공학관',
           '학생회관', '학술정보관', '제5공학관', '창업보육센터',
           '학연산 클러스터', '기숙사 셔틀콕']
 start_point = tk.ttk.Combobox(window, height=20, width = 10, values=values)
-start_point.place(x=210, y=200)
+start_point.place(x=210, y=190)
 start_point.set("출발지 선택")
 
 values.append('한대앞역')
 end_point = tk.ttk.Combobox(window, height=20, width = 10, values=values)
-end_point.place(x=310, y=200)
+end_point.place(x=310, y=190)
 end_point.set("도착지 선택")
 
 
 #search 버튼
 font = tk.font.Font(family="맑은 고딕", size = 10, weight='bold')
 search = tk.Button(window,text="SEARCH!",font=font,fg='White',bg='Red',command=View_path)
-search.place(x=410,y=195)
+search.place(x=410,y=185)
 
 tk.mainloop()
